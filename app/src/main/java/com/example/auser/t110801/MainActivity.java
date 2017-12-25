4149341959a6339976339d7bda25aba8d8a32bfe
7R <4a2k0055@stust.edu.tw>
2017-12-25 15:45:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/auser/t110801/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.auser.t110801;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<Map<String, Object>> mylist;
+    SimpleAdapter simpleAdapter;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+        mylist = new ArrayList<>();
+        HashMap<String, Object> m1 = new HashMap<>();
+        m1.put("city", "台北");
+        m1.put("code", "02");
+        m1.put("img", R.drawable.taipei);
+        HashMap<String, Object> m2 = new HashMap<>();
+        m2.put("city", "台中");
+        m2.put("code", "04");
+        m2.put("img", R.drawable.tc);
+        HashMap<String, Object> m3 = new HashMap<>();
+        m3.put("city", "高雄");
+        m3.put("code", "07");
+        m3.put("img", R.drawable.kh);
+        mylist.add(m1);
+        mylist.add(m2);
+        mylist.add(m3);
+
+        simpleAdapter = new SimpleAdapter(this, mylist, R.layout.myitem,
+                new String[] {"city", "code", "img"}, new int[] {R.id.textView, R.id.textView2, R.id.imageView});
+        listView.setAdapter(simpleAdapter);
+    }
+}
